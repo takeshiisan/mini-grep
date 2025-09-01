a4115b0a23116de5371dda7fc44b1f6ada36aadd
takeshiisan <takeshisan29@gmail.com>
2025-09-01 11:39:47 +0800

    fs error, need fix

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,7 @@
  * "grep" CLI Application/tool in unix systems
  */
 use std::env;
-
-
+use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
@@ -13,4 +12,8 @@ fn main() {
 
     println!("Searching for {}", query);
     println!("in file {}", filename);
+
+    let contents: String = fs::read_to_string(path: filename)
+        .expect(msg: "Something went wrong reading the file");
+    println!("With text:\n()", contents);
 }
