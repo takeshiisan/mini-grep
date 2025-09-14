e233fa4ad25ae30378667e2269b63059583e3e1f
takeshiisan <takeshisan29@gmail.com>
2025-09-14 16:00:53 +0800

    Redo

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,8 @@
  * "grep" CLI Application/tool in unix systems
  */
 use std::env;
-use std::fs;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
-
-    let query: &String = &args[1];
-    let filename: &String = &args[2];
-
-    println!("Searching for {}", query);
-    println!("in file {}", filename);
-
-    let contents: String = fs::read_to_string(path: filename)
-        .expect(msg: "Something went wrong reading the file");
-    println!("With text:\n()", contents);
+    dbg! (args);
 }
